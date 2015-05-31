76e383b54abc35ea7c50c060dfafd6f52f344f91
Ajani <scubaten@hotmail.com>
2015-05-31 18:19:09 -0400

    added sprites

--- js/colorfultaco.js
+++ js/colorfultaco.js
@@ -2,11 +2,12 @@
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas-container', { preload: preload, create: create, update: update });
 
     function preload() {
-
+        game.load.image('sky','img/sky.png');
+        game.load.image('taco','img/taco.png');
     }
 
     function create() {
-        
+        game.add.sprite(0, 0, 'taco');
     }
 
     function update() {
