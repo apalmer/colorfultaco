44016f6c300361c2381c3fed2da201b35c626de1
Ajani <scubaten@hotmail.com>
2015-05-31 18:46:23 -0400

    added ground

--- js/colorfultaco.js
+++ js/colorfultaco.js
@@ -4,11 +4,16 @@
     function preload() {
         game.load.image('sky','img/sky.png');
         game.load.image('taco','img/taco.png');
+        game.load.image('ground','img/ground.png');
     }
 
     function create() {
         game.add.sprite(0, 0, 'sky');
         game.add.sprite(0, 0, 'taco');
+        var length = 7;
+        for (var i = 0; i < length; i++) {   
+            game.add.sprite(120*i, 540, 'ground');
+        }
     }
 
     function update() {
